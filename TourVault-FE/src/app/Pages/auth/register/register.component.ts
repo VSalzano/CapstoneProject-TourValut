4c6b9e0b2dee7728c25b079606872691087de089
VSalzano <vincenzo.salzano@outlook.com>
2023-09-09 12:08:32 +0200

    form registrazione

--- TourVault-FE/src/app/Pages/auth/register/register.component.ts
+++ TourVault-FE/src/app/Pages/auth/register/register.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { IRegister } from 'src/app/Models/iregister';
+import { AuthService } from 'src/app/Services/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.sass']
+  styleUrls: ['./register.component.sass'],
 })
 export class RegisterComponent {
+  newUser: IRegister = {
+    name: '',
+    username: '',
+    email: '',
+    password: '',
+    telefono: '',
+    indirizzo: '',
+  };
 
+  constructor(private authSvc: AuthService, router: Router) {}
+
+  register() {
+    this.authSvc.register(this.newUser).subscribe((data) => {
+      console.log(data);
+    });
+  }
+
+  login() {}
 }
