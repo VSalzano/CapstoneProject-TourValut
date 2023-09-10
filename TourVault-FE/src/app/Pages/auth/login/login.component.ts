14540e9d75bb8148eb2d7dc955891dd00dd702e6
VSalzano <vincenzo.salzano@outlook.com>
2023-09-10 13:21:01 +0200

    login funzionante

--- TourVault-FE/src/app/Pages/auth/login/login.component.ts
+++ TourVault-FE/src/app/Pages/auth/login/login.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ILogin } from 'src/app/Models/ilogin';
+import { AuthService } from 'src/app/Services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.sass']
+  styleUrls: ['./login.component.sass'],
 })
 export class LoginComponent {
+  constructor(private router: Router, private authSvc: AuthService) {}
 
+  user: ILogin = {
+    username: '',
+    password: '',
+  };
+
+  login() {
+    this.authSvc.login(this.user).subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
