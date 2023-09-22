ed9353dfe3ac34f3a681e6dc6ad7011cfbc6a0b7
VSalzano <vincenzo.salzano@outlook.com>
2023-09-22 20:06:20 +0200

    grafica auth module finita

--- TourVault-FE/src/app/Pages/auth/register/register.component.ts
+++ TourVault-FE/src/app/Pages/auth/register/register.component.ts
@@ -6,7 +6,7 @@ import { AuthService } from 'src/app/Services/auth.service';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.sass'],
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
   newUser: IRegister = {
